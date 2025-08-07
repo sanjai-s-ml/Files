cde54d7d2ddd3e3f909e76149b768878f8e0b8bd
sanjai-s-ml <Sanjai.S@Xoriant.com>
2025-08-07 12:38:53 +0530

    Update perl.pl

--- perl.pl
+++ perl.pl
@@ -1,12 +1,9 @@
 use DBI;
 use strict;
 use warnings;
-my $dsn = "dbi:mysql:database=my_database_name:host=localhost";
-my $username = "my_username";
-my $password = "my_password";
-# Attempt to connect to the database
-my $dbh = DBI->connect($dsn, $username, $password)
-   or die "Could not connect to database: $DBI::errstr";
-print "Successfully connected to the database!\n";
-# Always remember to disconnect when you're done!
+my $dsn = "dbi:SQLite:dbname=test.db";
+my $dbh = DBI->connect($dsn, "", "") or die "Could not connect to database: $DBI::errstr";
+print "Successfully connected to the SQLite database!\n";
+$dbh->do("CREATE TABLE IF NOT EXISTS users (id INTEGER, name VARCHAR(255))");
+print "Created 'users' table!\n";
 $dbh->disconnect;
