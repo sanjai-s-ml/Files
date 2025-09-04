5210880db37b04092429822f02b06afdba29fe32
sanjai-s-ml <Sanjai.S@Xoriant.com>
2025-09-04 16:55:10 +0530

    Delete perl.pl

--- perl.pl
+++
@@ -1,9 +0,0 @@
-use DBI;
-use strict;
-use warnings;
-my $dsn = "dbi:SQLite:dbname=test.db";
-my $dbh = DBI->connect($dsn, "", "") or die "Could not connect to database: $DBI::errstr";
-print "Successfully connected to the SQLite database!\n";
-$dbh->do("CREATE TABLE IF NOT EXISTS users (id INTEGER, name VARCHAR(255))");
-print "Created 'users' table!\n";
-$dbh->disconnect;
